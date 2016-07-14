f1b479f3fd34fb3158573ace235695f7db8417fe
Mitch Sundt <mitchellsundt@gmail.com>
2016-07-14 11:34:09 -0700

    Add constants for row-level access control

---
+++ androidlibrary_lib/src/main/java/org/opendatakit/RoleConsts.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016 University of Washington
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.opendatakit;
+
+/**
+ * @author mitchellsundt@gmail.com
+ */
+public class RoleConsts {
+
+    /**
+     * There are two additional roles available for permissions modelling:
+     *
+     * ROLE_SYNCHRONIZE_TABLES -- ability to Sync (users with this role also have ROLE_USER)
+     * ROLE_SITE_ACCESS_ADMIN -- ability to create users on ODK Aggregate (also has all other roles)
+     *
+     */
+
+    /**
+     * Basic authenticated username or google account.
+     *
+     * If the user has the ODK 1.x Data viewer permission and no 2.0 permissions, they can have
+     * this role without the ability to Sync to the server.
+     *
+     * But, commonly, these users have ROLE_SYNCHRONIZE_TABLES
+     */
+    public static final String ROLE_USER = "ROLE_USER";
+
+    /**
+     * Ability to Sync, to alter row-level filters, and edit rows even if locked or restricted
+     */
+    public static final String ROLE_SUPER_USER = "ROLE_SUPER_USER_TABLES";
+
+    /**
+     * Ability to Reset App Server.
+     * Ability to Sync, to alter row-level filters, and edit rows even if locked or restricted
+     *
+     * Commonly, these users have ROLE_SITE_ACCESS_ADMIN
+     */
+    public static final String ROLE_ADMINISTRATOR = "ROLE_ADMINISTER_TABLES";
+}
