57bf97b11f48dd2ab83ff0d1497d741af93d08b1
Mitch Sundt <mitchellsundt@gmail.com>
2017-07-20 14:51:40 -0700

    when importing a csv file, if the row exists in the database and has conflicts or checkpoints, silently ignore the .csv content.
    This is needed to prevent the tools from force closing when they are restarted after being exited or force-closed during a survey or after a sync that leaves conflicts.

--- androidlibrary_lib/src/main/java/org/opendatakit/builder/CsvUtil.java
+++ androidlibrary_lib/src/main/java/org/opendatakit/builder/CsvUtil.java
@@ -589,14 +589,15 @@ public class CsvUtil {
             }
           }
 
-          // TODO: should resolve this properly when we have conflict rows and
-          // uncommitted edits. For now, we just add our csv import to those,
-          // rather than resolve the problems.
+          // if there are any conflicts or checkpoints on this row, we do not import
+          // this row change. Instead, silently ignore them.
           UserTable table = supervisor.getDatabase()
               .privilegedGetRowsWithId(appName, db, tableId, orderedDefns, v_id);
           if (table.getNumberOfRows() > 1) {
-            throw new IllegalStateException(
-                "There are either checkpoint or conflict rows in the destination table");
+            WebLogger.getLogger(appName).w(TAG,
+                "importSeparable: tableId: " + tableId + " rowId: " + v_id +
+                    " has checkpoints or conflicts -- IGNORED in .csv");
+            continue;
           }
 
           SyncState syncState = null;
