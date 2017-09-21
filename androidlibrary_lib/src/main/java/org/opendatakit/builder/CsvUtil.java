8456ed71869902e2bd65f75ca46ac391030f63f0
Mitch Sundt <mitchellsundt@gmail.com>
2017-09-21 12:09:54 -0700

    remove unused imports

--- androidlibrary_lib/src/main/java/org/opendatakit/builder/CsvUtil.java
+++ androidlibrary_lib/src/main/java/org/opendatakit/builder/CsvUtil.java
@@ -51,11 +51,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 /**
