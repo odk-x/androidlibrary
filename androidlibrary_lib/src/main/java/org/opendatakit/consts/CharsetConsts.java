0894514fcf232462aa66b4ea8d9c092bfddcd4f3
Mitch Sundt <mitchellsundt@gmail.com>
2017-09-21 12:13:07 -0700

    Add CharsetConsts for lint warning. Misc. changes for windows.
    Replace use of Charsets.UTF_8 everywhere with the one we define. Fiddly changes for Windows, and isolate license text parsing and add deprecation wanring suppression.

---
+++ androidlibrary_lib/src/main/java/org/opendatakit/consts/CharsetConsts.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2017 University of Washington
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.opendatakit.consts;
+
+import java.nio.charset.Charset;
+
+/**
+ * Added to suppress warnings about Charsets.UTF_8 deprecation
+ *
+ * @author mitchellsundt@gmail.com
+ */
+
+public class CharsetConsts {
+  public static final Charset UTF_8 = Charset.forName("UTF-8");
+}
