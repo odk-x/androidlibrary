237475665fc70935f2dc30d99c75cd75960bdb59
wbrunette <wbrunette@gmail.com>
2018-03-30 15:33:43 -0700

    make sure the show finishes when creating dialogs

--- androidlibrary_lib/src/main/java/org/opendatakit/fragment/ProgressDialogFragment.java
+++ androidlibrary_lib/src/main/java/org/opendatakit/fragment/ProgressDialogFragment.java
@@ -344,6 +344,7 @@ public class ProgressDialogFragment extends DialogFragment
          if(!outputProgressDialogFragment.isAdded()) {
             outputProgressDialogFragment.show(fragmentManager, progressDialogTag);
          }
+         fragmentManager.executePendingTransactions();
       } else {
          fragmentManager.executePendingTransactions();
          outputProgressDialogFragment.setTitle(title);
