645eb2f91d2e166f10965f33f3e11043cfef4216
Redeem Grimm <45304978+Redeem-Grimm-Satoshi@users.noreply.github.com>
2023-07-05 07:21:26 +0100

    Pref UI Fix (#252)
    * @wbrunette @maprehensive The changes here work in sync with the PR at ODK-Services.
    
    Totaling: #331 #332 #404 #400 #402
    odk-x/tool-suite-X#331
    odk-x/tool-suite-X#332
    odk-x/tool-suite-X#404
    odk-x/tool-suite-X#402
    odk-x/tool-suite-X#400
    
    * Indentation Adjustment
    
    * Sync Dialog UI Updates;
    
    1. Remove unneeded return statement in AlertDialogFragment
    2. Remove weird indentation in AlertDialogFragment
    3. Added X to OdkAlertDialogStyle
    4. Added new line at the end of each xml file to make github happy :)
    
    * Pref Screen UI Changes Issues Resolved.
    
    * Code Refactored
    
    * ...
    
    * Indentation Adjustment
    
    * Sync UI Updates;
    
    1. Dialog styling change to "OdkXAlertDialogStyle"
    2. Unnecessary return statement removed from AlertDialogFragment
    3. Added new line at the end of file for each xml file so github becomes happy :)
    
    * input textfield bg xml
    
    * Minor Fix ( New line at end of file )
    
    * Extended Color Palette
    
    * Minor Fixes
    
    1. All Dialogs at Pref Screen now have rounded corners
    
    * Minor Fixes
    
    1. All Dialogs at Pref Screen now have rounded corners
    2. Other fixes
    
    * Code Refactored
    
    1. Fragment package moved from Provider Package

--- androidlibrary_lib/src/androidTest/java/org/opendatakit/fragment/AlertNProgessMsgFragmentMgerTest.java
+++ androidlibrary_lib/src/androidTest/java/org/opendatakit/fragment/AlertNProgessMsgFragmentMgerTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertEquals;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.test.core.app.ActivityScenario;
+
 import org.opendatakit.test_utils.TestActivity;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import org.opendatakit.fragment.AlertNProgessMsgFragmentMger.DialogState;
